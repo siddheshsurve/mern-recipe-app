761f923825c2ede0da90ad95750894925759cb8c
siddheshsurve <sysurve24@gmail.com>
2023-07-16 18:18:48 +0530

    created pages and added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import Auth from './pages/Auth';
+import CreateRecipe from './pages/CreateRecipe';
+import SavedRecipe from './pages/SavedRecipe';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path />
+          <Route path="/" element={<Home/>} />
+          <Route path="/auth" element={<Auth/>} />
+          <Route path="/create-recipe" element={<CreateRecipe/>} />
+          <Route path="/saved-recipe" element={<SavedRecipe/>} />
         </Routes>
       </Router>
     </div>
---
+++ client/src/pages/SavedRecipe.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SavedRecipe = () => {
+  return (
+    <div>SavedRecipe</div>
+  )
+}
+
+export default SavedRecipe 
