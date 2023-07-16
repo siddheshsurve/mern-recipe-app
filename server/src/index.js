70c602441ea8c745b5e9d483fec546ab37b65f96
siddheshsurve <sysurve24@gmail.com>
2023-07-16 10:51:12 +0530

    setup express + nodemon

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,18 @@
 import express from "express";
-import cors from 'cors' 
+import cors from 'cors';
 import mongoose from "mongoose";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(3001, () => {
+    console.log("SERVER STARTED !");
+});
+
+
+
 
 
 
