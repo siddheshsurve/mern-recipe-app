f4cbfaedde5b636af05319e683c25fd040926d76
siddheshsurve <sysurve24@gmail.com>
2023-07-16 20:06:50 +0530

    setup Auth page

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Auth = () => {
   return (
-    <div>Auth</div>
+    <div className='auth'>
+        <Login />
+        <Register />
+    </div>
   )
+}
+
+const Login = () => {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    return (
+        <Form username={username} setUsername={setUsername} password={password} setPassword={setPassword} label="Login" />
+    )
+}
+
+const Register = () => {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    return (
+        <Form username={username} setUsername={setUsername} password={password} setPassword={setPassword} label="Register" />
+    )
+}
+
+const Form = ({username, setUsername, password, setPassword, label}) => {
+    return (
+        <div className='auth-container'>
+            <form>
+                <h2>{label}</h2>
+                <div className='form-group'>
+                    <label htmlFor="username">Username : </label>
+                    <input type='text' id="username" value={username} onChange={(event) => setUsername(event.target.value)} />
+                </div>
+                <div className='form-group'>
+                    <label htmlFor="password">Password : </label>
+                    <input type='text' id="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+                </div>
+
+                <button type='submit'>{label}</button>
+            </form>
+        </div>
+    )
 }
 
 export default Auth
