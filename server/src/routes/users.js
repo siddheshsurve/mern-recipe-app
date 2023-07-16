86e0b866a00922f138ac33915de78aff54c2feef
siddheshsurve <sysurve24@gmail.com>
2023-07-16 17:39:47 +0530

    login route : jwt sign

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -21,5 +21,23 @@ router.post("/register", async (req, res) => {
   res.json({message : "User Registered Successfully"});
 });
 
+router.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = await UserModel.findOne({ username });
+
+    if(!user) {
+        return res.json({message : "User Doesn't exist"});
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if(!isPasswordValid) {
+        return res.json({message : "Username or Password is incorrec!"});
+    }
+
+    const token = jwt.sign({id: user._id}, "secret");
+    res.json({token, userID : user._id});
+});
+
 export { router as userRouter };
 
