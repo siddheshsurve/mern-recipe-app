de1ee036b3a770aa079122eefe3f5479c3a24e8f
siddheshsurve <sysurve24@gmail.com>
2023-07-16 12:41:49 +0530

    creating routes for user

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from 'cors';
 import mongoose from "mongoose";
+import {userRouter} from './routes/users.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use("/auth", userRouter);
+
 
 mongoose.connect("mongodb+srv://admin:admin8390@recipe.lmagwiq.mongodb.net/");
 
