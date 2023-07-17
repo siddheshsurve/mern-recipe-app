b510e9522c4f5439fd94fa13776964d1326303c9
siddheshsurve <sysurve24@gmail.com>
2023-07-17 20:43:26 +0530

    logout button

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,17 +1,25 @@
 import React from 'react'
-import { Link } from "react-router-dom";
-import { useCookies } from 'react-cookie'; 
+import { Link, useNavigate } from "react-router-dom";
+import { useCookies } from 'react-cookie';
 
 const Navbar = () => {
 
-  const [cookies,]
+  const [cookies, setCookies] = useCookies(["access_token"]);
+  const navigate = useNavigate();
+
+  const logout = () => {
+    setCookies("access_token", "");
+    window.localStorage.removeItem("userID");
+    navigate("/auth");
+  }
 
   return (
     <div className='navbar'>
-        <Link to="/">Home</Link>
-        <Link to="/create-recipe">Create Recipe</Link>
-        <Link to="/saved-recipe">Saved Recipe</Link>
-        <Link to="/auth">Login/Register</Link>
+      <Link to="/">Home</Link>
+      <Link to="/create-recipe">Create Recipe</Link>
+      <Link to="/saved-recipe">Saved Recipe</Link>
+      {!cookies.access_token ? (<Link to="/auth">Login/Register</Link>) : <button onClick={logout}>Logout</button>}
+
     </div>
   )
 }
